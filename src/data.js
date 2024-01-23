d2a146a02ccd917ac32b8d2a8d43262093d577af
Sir1ys <sashakoroviy@gmail.com>
2024-01-23 16:30:44 +0000

    initial data is created

---
+++ src/data.js
@@ -0,0 +1,228 @@
+export const Bio = {
+  name: "Oleksandr Korovii",
+  roles: ["Full Stack Developer"],
+  description:
+    "Driven by ambition and a deep passion for working with computers, I've taken proactive measures to pivot my career path towards becoming a developer. My progress and achievements since embarking on this tech journey highlight my potential, fueling my motivation to persistently learn and expand my skills. Engaging in paired programming, I adeptly followed Agile ethodologies and embraced Test-Driven Development (TDD) as integral components of my practical experience",
+  github: "https://github.com/Sir1ys",
+  resume:
+    "https://drive.google.com/file/d/1MG2F8FYyK2SSfSTwDHJW5aiQyi2d-Sy5/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/oleksandr-korovii-7b293a229/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://drive.google.com/file/d/1-kij11FIH1LagMwiNoVCcTz3kx61FAdn/view?usp=drive_link",
+      },
+      {
+        name: "React Native",
+        image:
+          "https://drive.google.com/file/d/15A0eDmkgEjGGQc732ZOsW0Pfyx4iyfdR/view?usp=drive_link",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://drive.google.com/file/d/1VdJD6HmC4nI2yVeZ7f1JlHpqfm3Gm3_q/view?usp=drive_link",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://drive.google.com/file/d/1bkHdMwezpIMeqajyWyqnKCKiLBUFO5FB/view?usp=drive_link",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://drive.google.com/file/d/1AjBsOOHrV8YYfy6cuMkp_8xS1R8eRHdg/view?usp=drive_link",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://drive.google.com/file/d/1sdHGfNNV_4xPG7lAe4tr4k8-ZVP69mUJ/view?usp=drive_link",
+      },
+      {
+        name: "Sass",
+        image:
+          "https://drive.google.com/file/d/1ZBrg6HPSyKsyG5jCd_72lI6CXrVI9i4Q/view?usp=drive_link",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://drive.google.com/file/d/1-OerRQ20YjW8PXBENjtYkJjiiInj8pR9/view?usp=drive_link",
+      },
+      {
+        name: "HTML",
+        image:
+          "https://drive.google.com/file/d/1dgoMkpDlkkpPLVTdlMalHlW3FcjX4WFO/view?usp=drive_link",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image:
+          "https://drive.google.com/file/d/1nMAoLD9Fc4oPzsmHnPlQcE5eTapl_zWg/view?usp=drive_link",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://drive.google.com/file/d/1gXGL7fez92geK-h09YmK_1ywYthqiOIx/view?usp=drive_link",
+      },
+      {
+        name: "Jest",
+        image:
+          "https://drive.google.com/file/d/1Ruyy9iS4SUQeGAPY5x0SNKt82_r_kedi/view?usp=drive_link",
+      },
+      {
+        name: "Postgresql",
+        image:
+          "https://drive.google.com/file/d/1tEJwut7WH7PR3I6REoVNRUu6nMoIHdkq/view?usp=drive_link",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://drive.google.com/file/d/1RTaPYX5vTh9dzAtaK5siS1g8SAtQrR0A/view?usp=drive_link",
+      },
+      {
+        name: "Firebase",
+        image:
+          "https://drive.google.com/file/d/1pzhzd3zDmv9rPUTWYfX1jtMGVs0sHND4/view?usp=drive_link",
+      },
+      {
+        name: "Axios",
+        image:
+          "https://drive.google.com/file/d/1uoaV8HXCY8is4VjQf7OimkcHqpB8TI4l/view?usp=drive_link",
+      },
+      {
+        name: "Validate.js",
+        image:
+          "https://drive.google.com/file/d/1lDveVL4VIO1wRFGufzpmITUVyX4YLTs2/view?usp=drive_link",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://drive.google.com/file/d/1o3hfC1NwWQ4I0foKg4AGZOil-sfode5M/view?usp=drive_link",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://drive.google.com/file/d/1roGCJhBY_92nOfHmawlO8uFB99adjRYe/view?usp=drive_link",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://drive.google.com/file/d/1NUMlrLbb8IS2-bHxjHuCWpjRF3Wov0VL/view?usp=drive_link",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://drive.google.com/file/d/1oRKBCnTTZgXzdMpIrVgMQfUsYarNOY0l/view?usp=drive_link",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://drive.google.com/file/d/1cIJwMHk4Ad4pcOsPTI9oBRao8AGLNo-b/view?usp=drive_link",
+    school: "Northcoders, Manchester",
+    date: "Aug 2023 - Nov 2023",
+    desc: "While at Northcoders, I practiced paired programming with Agile methodologies. I gained modern software development skills through collaborative coding, iterative development, and a test-driven approach in web application development. Proficient in constructing web servers, I effectively worked with various databases. Additionally, I mastered the React framework, utilizing its features adeptly.",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Collab Hub",
+    date: "Nov 2023 - Jan 2024",
+    description:
+      "a collaborative project with a friend aimed at addressing the challenge developers face in finding the right team members for their projects.",
+    image:
+      "https://drive.google.com/file/d/1xj5buHWTI2v9I5tUNaMq4m5AxIIbxV33/view?usp=drive_link",
+    tags: {
+      backEnd: [
+        "Axios",
+        "Postgresql",
+        "Node Js",
+        "Express Js",
+        "Jest",
+        "Javascript",
+        "TypeScript",
+      ],
+      frontEnd: ["React", "TypeScript", "Tailwind", "Axios"],
+    },
+    category: "back-end & front-end",
+    github_be: "https://github.com/JDean8/collab-hub-be",
+    github_fe: "https://github.com/Sir1ys/collab-hub-fe",
+    webapp_be: "https://collub-hub.onrender.com/api/users",
+    webapp_fe: "https://collub-hub.onrender.com/api/users",
+    stage: "in progress",
+  },
+  {
+    id: 1,
+    title: "Junket",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "a group project to address the challenge of solo travel. Our aim was to create a mobile app that makes it easier to find friends and events based on your destination.",
+    image:
+      "https://drive.google.com/file/d/1T3tzTIemU2LuNq4uhT2Ma4KVlrJ60YcF/view?usp=drive_link",
+    tags: {
+      backEnd: [
+        "Supertest",
+        "Validate.js",
+        "Axios",
+        "Postgresql",
+        "Node Js",
+        "Express Js",
+        "Jest",
+        "Javascript",
+      ],
+      frontEnd: ["React Native", "Expo Go", "Axios", "Firebase"],
+    },
+    category: "back-end & front-end",
+    github_be: "https://github.com/JDean8/codecrafters-be",
+    github_fe: "https://codecrafters-9qyn.onrender.com/api",
+    webapp_be: "https://github.com/Sir1ys/codecrafters-fe",
+    webapp_fe: "https://northcoders.com/project-phase/junket/",
+    stage: "finished",
+  },
+  {
+    id: 2,
+    title: "Nc-News",
+    date: "Sep 2023 - Oct 2023",
+    description:
+      "a web application that I developed independently. It is designed for individuals to read articles and engage in discussions with others.",
+    image:
+      "https://drive.google.com/file/d/1jLvbuVXpRF7NVowiFsIQj9H8vtthyg7Z/view?usp=drive_link",
+    tags: {
+      backEnd: [
+        "Supertest",
+        "Axios",
+        "Postgresql",
+        "Node Js",
+        "Express Js",
+        "Jest",
+        "Javascript",
+      ],
+      frontEnd: ["React", "MUI", "Axios", "Sass"],
+    },
+    category: "back-end & front-end",
+    github_be: "https://github.com/Sir1ys/back-end-news",
+    github_fe: "https://back-end-news.onrender.com/api",
+    webapp_be: "https://github.com/Sir1ys/fe-nc-news",
+    webapp_fe: "https://calm-gelato-6eb82e.netlify.app/",
+    stage: "finished",
+  },
+];
