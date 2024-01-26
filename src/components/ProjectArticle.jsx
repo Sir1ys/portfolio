734dbb1c410d802edcfe3046360f0dc48ad4cbaa
Sir1ys <sashakoroviy@gmail.com>
2024-01-26 20:34:59 +0000

    reusable modal is created

--- src/components/ProjectArticle.jsx
+++ src/components/ProjectArticle.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 export default function ProjectArticle({
   title,
@@ -13,16 +14,27 @@ export default function ProjectArticle({
   webapp_fe,
   stage,
 }) {
+  const [active, setActive] = useState(false);
+
   return (
-    <article className="flex flex-col items-center justify-between gap-2 text-center text-emerald-50 border rounded-lg cursor-pointer transition duration-500 ease-in-out transform hover:scale-105">
-      <img
-        className="w-full h-64 rounded-lg"
-        src={image}
-        alt={`The picture of ${title} project.`}
-      />
-      <h3 className="font-semibold tracking-wider text-xl">{title}</h3>
-      <span>{date}</span>
-      <span className="text-lime-50 p-2">{description}</span>
-    </article>
+    <>
+      <article
+        className="flex flex-col items-center justify-between gap-2 text-center text-emerald-50 border border-emerald-300 rounded-lg cursor-pointer transition duration-500 ease-in-out transform hover:scale-105 z-1"
+        onClick={() => setActive(true)}
+      >
+        <img
+          className="w-full h-64 rounded-lg"
+          src={image}
+          alt={`The picture of ${title} project.`}
+        />
+        <h3 className="font-semibold tracking-wider text-xl">{title}</h3>
+        <span>{date}</span>
+        <span className="text-lime-50 p-2">{description}</span>
+      </article>
+
+      <Modal active={active} setActive={setActive}>
+        <p>Hi</p>
+      </Modal>
+    </>
   );
 }
