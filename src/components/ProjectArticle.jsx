556ee660e5fb0e14b5dfa8fc2c40b338581a5389
Sir1ys <sashakoroviy@gmail.com>
2024-01-26 20:28:08 +0000

    initial styles for projects section are applied

--- src/components/ProjectArticle.jsx
+++ src/components/ProjectArticle.jsx
@@ -14,7 +14,7 @@ export default function ProjectArticle({
   stage,
 }) {
   return (
-    <article className="flex-item w-10/12 xl:w-1/2 flex flex-col items-center justify-between gap-2 text-center text-emerald-50 border rounded-lg cursor-pointer transition duration-500 ease-in-out transform hover:scale-105">
+    <article className="flex flex-col items-center justify-between gap-2 text-center text-emerald-50 border rounded-lg cursor-pointer transition duration-500 ease-in-out transform hover:scale-105">
       <img
         className="w-full h-64 rounded-lg"
         src={image}
@@ -22,15 +22,6 @@ export default function ProjectArticle({
       />
       <h3 className="font-semibold tracking-wider text-xl">{title}</h3>
       <span>{date}</span>
-      {/* <div className="flex gap-1 flex-wrap justify-center">
-        {skills
-          .map((skill) => (
-            <span className="p-1 border border-emerald-800 rounded-md">
-              {skill}
-            </span>
-          ))
-          .slice(0, 6)}
-      </div> */}
       <span className="text-lime-50 p-2">{description}</span>
     </article>
   );
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -6,13 +6,12 @@ export default function ProjectsSection() {
   return (
     <section
       id="projects"
-      className="w-11/12 xl:w-9/12 text-center flex items-center justify-between gap-4 p-8 flex-col"
+      className="w-11/12 xl:w-9/12 text-center p-8 flex flex-col items-center gap-8"
     >
       <h2 className="font-semibold text-white text-4xl tracking-widest text-center">
         Projects
       </h2>
-
-      <div className="flex items-center flex-wrap flex-col xl:flex-row gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
         {projects.map((project) => (
           <ProjectArticle key={project.id} {...project} />
         ))}
