54882449ef6e44ccde40c88ecaabdd1117621105
Sir1ys <sashakoroviy@gmail.com>
2024-01-27 16:39:48 +0000

    styles for project window are updated

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,15 +1,7 @@
-import { skills } from "../../data";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Modal({ active, setActive, project }) {
-  const { title, date, description, image, skills: prSkills, links } = project;
-
-  const allSkillsArrays = skills.map((skillList) => skillList.skills);
-
-  const allSkills = allSkillsArrays.flat();
-
-  const projectSkills = allSkills.filter((skill) =>
-    prSkills.includes(skill.name)
-  );
+  const { title, date, description, image, skills, links } = project;
 
   return (
     <div
@@ -20,30 +12,36 @@ export default function Modal({ active, setActive, project }) {
         setActive(false);
       }}
     >
-      <div
-        className="rounded-2xl bg-gray-50 transition-all delay-200 w-10/12 md:w-6/12 border-2 border-emerald-500"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="rounded-2xl bg-gray-50 transition-all delay-200 w-10/12 md:w-6/12 border-2 border-emerald-500">
         <article
           className="flex flex-col items-center justify-between gap-2 text-center text-emerald-800 rounded-2xl transition duration-500 ease-in-out transform"
           onClick={() => setActive(true)}
         >
+          <span
+            onClick={() => setActive(false)}
+            className="md:hidden absolute top-1 right-1 self-end p-1 rounded-xl bg-emerald-900 border-2 border-emerald-300 text-white cursor-pointer"
+          >
+            <CloseIcon fontSize="large" />
+          </span>
           <img
             className="rounded-2xl"
             src={image}
             alt={`The picture of ${title} project.`}
           />
-          <div className="p-4 flex flex-col justify-center items-center gap-3">
+          <div
+            className="p-4 flex flex-col justify-center items-center gap-3"
+            onClick={(e) => e.stopPropagation()}
+          >
             <h3 className="font-semibold tracking-wider text-2xl">{title}</h3>
             <span>{date}</span>
             <ul className="flex gap-2 flex-wrap justify-center items-center">
-              {projectSkills.map((skill) => {
+              {skills.map((skill) => {
                 return (
                   <li
-                    key={skill.name}
-                    className="w-16 h-16 p-4 bg-gray-50 flex flex-row justify-center items-center border-2 border-emerald-500 rounded-md cursor-pointer "
+                    key={skill}
+                    className="w-32 p-1 bg-gray-50 flex flex-row justify-center items-center border-2 border-emerald-500 rounded-md cursor-pointer "
                   >
-                    {skill.image}
+                    {skill}
                   </li>
                 );
               })}
