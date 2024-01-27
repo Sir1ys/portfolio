94cd89d01fe6c8c6a3e7181aedf9e0143c9afb78
Sir1ys <sashakoroviy@gmail.com>
2024-01-27 16:49:40 +0000

    scrolling effect is added for mobile devices

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -5,66 +5,55 @@ export default function Modal({ active, setActive, project }) {
 
   return (
     <div
-      className={`z-50 h-screen w-screen bg-emerald-950 fixed top-0 left-0 flex items-center justify-center opacity-0 pointer-events-none duration-200 ${
+      className={`z-50 fixed inset-0 flex items-center justify-center bg-emerald-950 opacity-0 pointer-events-none duration-200 ${
         !active ? "scale-0" : "scale-100 opacity-100 pointer-events-auto"
       }`}
-      onClick={() => {
-        setActive(false);
-      }}
+      onClick={() => setActive(false)}
     >
-      <div className="rounded-2xl bg-gray-50 transition-all delay-200 w-10/12 md:w-6/12 border-2 border-emerald-500">
-        <article
-          className="flex flex-col items-center justify-between gap-2 text-center text-emerald-800 rounded-2xl transition duration-500 ease-in-out transform"
-          onClick={() => setActive(true)}
+      <div className="relative bg-gray-50 border-2 border-emerald-500 rounded-2xl transition-all delay-200 w-10/12 md:w-6/12 max-h-full overflow-auto">
+        <span
+          onClick={() => setActive(false)}
+          className="absolute top-1 right-1 p-1 bg-emerald-900 border-2 border-emerald-300 text-white rounded-xl cursor-pointer z-50"
         >
-          <span
-            onClick={() => setActive(false)}
-            className="md:hidden absolute top-1 right-1 self-end p-1 rounded-xl bg-emerald-900 border-2 border-emerald-300 text-white cursor-pointer"
-          >
-            <CloseIcon fontSize="large" />
-          </span>
+          <CloseIcon fontSize="large" />
+        </span>
+        <div
+          className="flex flex-col items-center gap-2 text-center text-emerald-800 rounded-2xl p-4"
+          onClick={(e) => e.stopPropagation()}
+        >
           <img
             className="rounded-2xl"
             src={image}
             alt={`The picture of ${title} project.`}
           />
-          <div
-            className="p-4 flex flex-col justify-center items-center gap-3"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <h3 className="font-semibold tracking-wider text-2xl">{title}</h3>
-            <span>{date}</span>
-            <ul className="flex gap-2 flex-wrap justify-center items-center">
-              {skills.map((skill) => {
-                return (
-                  <li
-                    key={skill}
-                    className="w-32 p-1 bg-gray-50 flex flex-row justify-center items-center border-2 border-emerald-500 rounded-md cursor-pointer "
-                  >
-                    {skill}
-                  </li>
-                );
-              })}
-            </ul>
-            <span>{description}</span>
-            <ul className="flex flex-row flex-wrap justify-center items-center gap-5">
-              {links.map((link) => {
-                return (
-                  <li key={link.name} className="text-sm md:text-md">
-                    <a
-                      href={link.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="p-2 text-sky-600 hover:text-sky-800 border border-sky-600 rounded-xl hover:bg-sky-200 cursor-pointer "
-                    >
-                      {link.name}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </article>
+          <h3 className="font-semibold tracking-wider text-2xl">{title}</h3>
+          <span>{date}</span>
+          <ul className="flex gap-2 flex-wrap justify-center items-center">
+            {skills.map((skill) => (
+              <li
+                key={skill}
+                className="w-32 p-1 border-2 border-emerald-500 rounded-md cursor-pointer"
+              >
+                {skill}
+              </li>
+            ))}
+          </ul>
+          <span>{description}</span>
+          <ul className="flex flex-row flex-wrap justify-center items-center gap-5">
+            {links.map((link) => (
+              <li key={link.name} className="text-sm md:text-md">
+                <a
+                  href={link.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-2 text-sky-600 hover:text-sky-800 border border-sky-600 rounded-xl hover:bg-sky-200 cursor-pointer"
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
